4c0d9ca3fc9918ac268d1fc6131f0dffcde0b4b8
aczee <aczee@ADAM>
2026-03-04 10:03:04 +0100

    Add project files.

---
+++ bingoWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace bingoWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
